f56255b6fadac6c3fa978d9429db23208f61f530
Matthew Clifford <mhmcliff@gmail.com>
2023-01-24 19:04:22 -0500

    Add barriers to window

--- main.py
+++ main.py
@@ -6,19 +6,21 @@ pygame.init()
 clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((1200, 560))
+health_font = pygame.font.Font('Pixeltype.ttf', 50)
 background = pygame.image.load("background.jpg")
 background = pygame.transform.rotozoom(background, 0, 0.2)
 
 # Player
-player_surf = pygame.image.load("player_sprites/player_right.png")
-player_rect = player_surf.get_rect(bottomright = (425, 495))
-player_surf = pygame.transform.rotozoom(player_surf, 0, 2.5)
+player_surf = pygame.image.load("player_sprites/player_right.png").convert_alpha()
+player_rect = player_surf.get_rect(bottomright = (575, 495))
+player_surf = pygame.transform.rotozoom(player_surf, 0, 2.5).convert_alpha()
 player_gravity = 0
-
+player_health = 50
+health_display = health_font.render(f"Health: {player_health} ", False, (0, 0, 0))
 # Enemy #1
-enemy1_surf = pygame.image.load("enemy1sprites/enemy1_right.png")
-enemy1_rect = enemy1_surf.get_rect(bottomleft = (900, 485))
-enemy1_surf = pygame.transform.rotozoom(enemy1_surf, 0, 2.5)
+enemy1_surf = pygame.image.load("enemy1sprites/enemy1_right.png").convert_alpha()
+enemy1_rect = enemy1_surf.get_rect(bottomleft = (1100, 485))
+enemy1_surf = pygame.transform.rotozoom(enemy1_surf, 0, 2.5).convert_alpha()
 
 # Delay between enemy attack
 attack_delay = 1
@@ -32,7 +34,7 @@ while True:
     screen.blit(background, (0, 0))
     screen.blit(player_surf, player_rect)
     screen.blit(enemy1_surf, enemy1_rect)
-
+    screen.blit(health_display, (550,50))
 # Player Movement
     if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_LEFT:
@@ -43,25 +45,30 @@ while True:
             player_surf = pygame.image.load("player_sprites/player_right.png")
             player_surf = pygame.transform.rotozoom(player_surf, 0, 2.5)
             player_rect.x += 3.5
+        if player_rect.x >= 1075:
+            player_rect.x = 1075
+        elif player_rect.x <= 0:
+            player_rect.x = 0
 # Player Jump
-        """if event.key == pygame.K_UP and player_rect.bottom >= 495:
-            player_rect.y -= player_gravity"""
 
+    # Enemy 1 collision
+    if enemy1_rect.x == player_rect.x:
+        player_health -= 0.05
 
-
-
-# Enemy 1 collision
-    #if enemy1_rect.colliderect(player_rect):
-        #enemy1_rect.x = player_rect.x
-        #print("collision")
+    if int(player_health) == 0:
+        exit()
 
     #player_surf = pygame.image.load("player_sprites/player_jump.png")
     #player_surf = pygame.transform.rotozoom(player_surf, 0, 2.5)
-
+# Enemy 1 AI
     if player_rect.x != enemy1_rect.x:
         if player_rect.x > enemy1_rect.x:
+            enemy1_surf = pygame.image.load("enemy1sprites/enemy1_right.png")
+            enemy1_surf = pygame.transform.rotozoom(enemy1_surf, 0, 2.5)
             enemy1_rect.x += 1
         elif player_rect.x < enemy1_rect.x:
+            enemy1_surf = pygame.image.load("enemy1sprites/enemy1_left.png")
+            enemy1_surf = pygame.transform.rotozoom(enemy1_surf, 0, 2.5)
             enemy1_rect.x -= 1
 
     pygame.display.update()
